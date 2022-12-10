7ea5c016a62fdb40ffc1fe30d97a9d12055596eb
SillySoon <silly@sillysoon.de>
2022-12-10 13:37:30 +0100

    🚀 Finished routing.js constructor
    Big update for the website, now only the products.csv has to be filled and the server restarted to add new items. the routing.js constructor fully works but still needs some tweeks.

--- app/routing.js
+++ app/routing.js
@@ -19,78 +19,71 @@ const requestListener = function(req, res) {
             res.end(prepairINDEX());
             break;
 
-        case '/spyro-reignited-trilogy':
-            //  game path => /spyro-reignited-trilogy
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.end(prepairHTML(7));
-            break;
-
-        case '/vrchat':
-            //  game path => /vrchat
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.end(prepairHTML(7));
-            break;
-
-        case '/dead-by-daylight':
-            //  game path => /dead-by-daylight
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.end(prepairHTML(6));
-            break;
-
-        case '/stray':
-            //  game path => /stray
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.end(prepairHTML(5));
-            break;
-
-        case '/cyberpunk-2077':
-            //  game path => /cyberpunk-2077
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.end(prepairHTML(4));
-            break;
-
-        case '/war-thunder':
-            //  game path => /war-thunder
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.end(prepairHTML(3));
-            break;
-
-        case '/overcooked!-2':
-            //  game path => /overcooked!-2
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.end(prepairHTML(2));
-            break;
-
-        case '/thehunter-call-of-the-wild':
-            //  game path => /thehunter-call-of-the-wild
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.end(prepairHTML(1));
-            break;
-
         case '/anno-1800':
             //  game path => /anno-1800
             res.writeHeader(200, { 'Content-Type': 'text/html' });
             res.end(prepairHTML(0));
             break;
-
-        case '/public/images/home.png':
-            //  image path => /home.png
-            res.writeHeader(200, { 'Content-Type': 'image/png' });
-            res.end(fs.readFileSync('./public/images/home.png'));
+        case '/thehunter-call-of-the-wild':
+            //  game path => /thehunter-call-of-the-wild
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML(1));
             break;
-
+        case '/overcooked!-2':
+            //  game path => /overcooked!-2
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML(2));
+            break;
+        case '/war-thunder':
+            //  game path => /war-thunder
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML(3));
+            break;
+        case '/cyberpunk-2077':
+            //  game path => /cyberpunk-2077
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML(4));
+            break;
+        case '/stray':
+            //  game path => /stray
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML(5));
+            break;
+        case '/dead-by-daylight':
+            //  game path => /dead-by-daylight
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML(6));
+            break;
+        case '/vrchat':
+            //  game path => /vrchat
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML(7));
+            break;
+        case '/spyro-reignited-trilogy':
+            //  game path => /spyro-reignited-trilogy
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML(8));
+            break;
+        case '/assassin's-creed-valhalla':
+            //  game path => /assassin's-creed-valhalla
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML(9));
+            break;
         case '/public/images/github.png':
             //  image path => /github.png
             res.writeHeader(200, { 'Content-Type': 'image/png' });
             res.end(fs.readFileSync('./public/images/github.png'));
             break;
-
+        case '/public/images/home.png':
+            //  image path => /home.png
+            res.writeHeader(200, { 'Content-Type': 'image/png' });
+            res.end(fs.readFileSync('./public/images/home.png'));
+            break;
         case '/style-dark.css':
             //  css path => /style-dark.css
             res.writeHeader(200, { 'Content-Type': 'text/css' });
             res.end(fs.readFileSync('./public/css/style-dark.css', "utf-8"));
             break;
-
         default:
             //  default error404 path => /404.html
             res.writeHeader(404, { 'Content-Type': 'text/html' });
--- test/prepairRouting.js
+++ test/prepairRouting.js
@@ -4,35 +4,69 @@
 const fs = require('fs');
 const logger = require('silly-logger');
 
+//  Read products.csv, split at \n and shift one line.
+const productData = fs.readFileSync('./public/csv/products.csv', 'UTF8');
+const products = productData.split('\n');
+products.shift();
+
+
+/*
+    function refreshRouting
+    This function will refresh the routing.js so it automatically changes to the
+    data in the csv file. This code will be run once the programm starts to add the new routes.
+*/
 const refreshRouting = function() {
 
-    const document = fs.readFileSync('./test/routingTemplate.js', 'utf-8');
+    //  read the template and predefine data, we will merge the document with the data later
+    let document = fs.readFileSync('./test/routingTemplate.txt', 'utf-8');
 
     let data = '';
 
-    // testing Data TODO: doing great code work here.
-    data = data + `
-        case '/game':
-            //  template for game files
-            res.writeHeader(200, { 'Content-Type': 'text/html' });
-            res.end(prepairHTML(0));
-            break;
-    `;
+    // Here we split the scv again, to get the individual part of the csv that we need.
+    const splitEachProduct = record => {
+        const fields = record.split(";");
 
-    //  photo files
+        //  reads the productExample.html - a template for a product view on a website
+        let html = `
+        case '/{path}':
+            //  game path => /{path}
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            res.end(prepairHTML({number}));
+            break;`;
+
+        //  replacing the {path} argument so that we have out /root ending of the link eg /anno-1800
+        html = html.replaceAll('{path}', fields[0].toLowerCase().replace(/ /g, "-"));
+
+        //  replaces the {number} with the game ID
+        //  TODO: when the number is not in order the code will break
+        html = html.replaceAll('{number}', fields[2]);
+
+
+        //  returns the html
+        return html;
+    };
+
+    const pages = products
+        .filter(row => row !== "")
+        .map(splitEachProduct);
+
+    pages.forEach(page => {
+        data = data + page;
+    });
+
+    //  prepair photo files part
     const imagesFolder = fs.readdirSync('./public/images/');
 
     imagesFolder.forEach(element => {
         data = data + `
-        case '/${element}':
+        case '/public/images/${element}':
             //  image path => /${element}
             res.writeHeader(200, { 'Content-Type': 'image/png' });
             res.end(fs.readFileSync('./public/images/${element}'));
-            break;
-        `;
+            break;`;
     });
 
-    //  css files
+    //  prepair css files part
     const cssFolder = fs.readdirSync('./public/css/');
 
     cssFolder.forEach(element => {
@@ -41,12 +75,19 @@ const refreshRouting = function() {
             //  css path => /${element}
             res.writeHeader(200, { 'Content-Type': 'text/css' });
             res.end(fs.readFileSync('./public/css/${element}', "utf-8"));
-            break;
-        `;
+            break;`;
     });
 
-    logger.debug(document.replace('${extraCases}', data));
+    // merging the document with data, for that de document contains ${extraCases} so that we can easiely replace it
+    document = document.replace('${extraCases}', data);
 
+    // remove the current routing file and renew it
+    //  TODO: nodemon (npm run start:dev) restarts every time the file changes, blacklist the routing.js to make it work
+    fs.rmSync('./app/routing.js');
+    logger.custom('Remove', 'red', '', 'Removed ./app/routing.js');
+
+    fs.writeFileSync('./app/routing.js', document);
+    logger.success('Wrote ./app/routing.js');
 };
 
 module.exports = refreshRouting;
